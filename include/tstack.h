f5498da82b91179199567272e4cd0297e3babd8b
kghstgrl <160274071+kghstgrl@users.noreply.github.com>
2024-04-06 22:00:37 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,10 +4,10 @@
 
 template<typename T, int size>
 class TStack {
-private:
+ private:
     T* stackArray;
     int topIndex;
-public:
+ public:
     TStack() : topIndex(-1) {
         stackArray = new T[size];
     }
